dedb579f2b7d23a495f0b545e8224abb78057138
georgethomas97 <59157039+georgethomas97@users.noreply.github.com>
2021-10-20 21:59:46 +0530

    Console Syntax Update

--- GuessingProgram/src/test/java/com/game/guesser/service/GuessingServiceImplTest.java
+++ GuessingProgram/src/test/java/com/game/guesser/service/GuessingServiceImplTest.java
@@ -12,8 +12,8 @@ public class GuessingServiceImplTest {
         assertEquals("Your first guess is: 7 (cold)\n", guessingService.processGuess(1, 7, 0));
         assertEquals("Your second guess is: 3 (warm)\n", guessingService.processGuess(1, 3, 1));
         assertEquals("Your second guess is: 2 (hot)\n", guessingService.processGuess(1, 2, 1));
-        assertEquals("Your third guess is: 2 (hot)\nSorry! You were unable to guess the number.", guessingService.processGuess(1, 2, 2));
-        assertEquals("Your third guess is: 7.\nRight! You have won the game.", guessingService.processGuess(7,7,2));
+        assertEquals("Your last guess is: 2 (hot)\nSorry! You were unable to guess the number.", guessingService.processGuess(1, 2, 2));
+        assertEquals("Your last guess is: 7.\nRight! You have won the game.", guessingService.processGuess(7,7,2));
 
     }
 }
